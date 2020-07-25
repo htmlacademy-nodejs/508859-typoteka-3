8e1bbb2a1aa40ed230edc3cd05c62e23ed6b99c9
JIlyaS <ilkolmakov@yandex.ru>
2020-07-25 15:19:35 +0500

    change http on express server

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,51 +2,39 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
 
 const {FILE_NAME, HttpCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 
+const app = express();
+app.use(express.json());
 
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
+let fileContent;
 
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
-
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
+fs.access(`./${FILE_NAME}`)
+  .then(async () => {
+    fileContent = await fs.readFile(`./${FILE_NAME}`);
+  })
+  .catch(async () => {
+    fileContent = JSON.stringify([]);
+    await fs.writeFile(`./${FILE_NAME}`, fileContent);
   });
 
-  res.end(template);
-};
+app.get(`/posts`, (req, res) => {
+  try {
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+  }
+});
+
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
@@ -54,14 +42,17 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
+    try {
+      app.listen(port, (err) => {
         if (err) {
           return console.error(`Ошибка при создании сервера`, err);
         }
 
         return console.info(chalk.green(`Ожидаю соединений на ${port}`));
       });
+    } catch (err) {
+      console.error(`Произошла ошибка: ${err.message}`);
+      process.exit(1);
+    }
   }
 };
